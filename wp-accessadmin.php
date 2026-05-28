c85105deb4b650a9e1906f8282aa23dc8deaea0d
domoquick <carlos.paz@domoquick.fr>
2026-05-28 13:07:35 +0200

    chore: bump version 1.0.1

--- wp-accessadmin.php
+++ wp-accessadmin.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * Plugin Name: WP Access Admin
  * Plugin URI:  https://github.com/domoquick/wp_accessadmin
  * Description: Corrige les URLs login/logout/admin lorsque WordPress est installé dans un sous-répertoire (pattern Bedrock). Remplace la base site_url() par home_url() dans les URLs générées.
- * Version:     1.0.0
+ * Version:     1.0.1
  * Requires at least: 6.0
  * Requires PHP: 8.3
  * Author:      Domoquick
